e4047d8b3ef9186104f2427b1e04448defa8a3e5
Oleg Kurnyavko <kurnyavko@gmail.com>
2025-11-28 12:20:40 +0600

    Настройки сервера вынесены в переменные окружения, которые подключены с помощью пакета @nestjs/config.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('APP_PORT', 80);
+  const host = configService.get<string>('APP_HOST', 'localhost');
+
+  await app.listen(port, host, () => {
+    console.log(`Сервер начал прослушивать хост ${host} на порту ${port}...`);
+  });
+
 }
 bootstrap();
